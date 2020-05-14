af1d6895712878ccff94eaa464966581e0dc48fa
quhongtao <quhongtao@hdkjrd.com>
2020-05-14 22:57:35 +0800

    over

--- src/views/Personal/components/Seller5AddCard.vue
+++ src/views/Personal/components/Seller5AddCard.vue
@@ -11,35 +11,45 @@
       <el-row>
         <el-col :span="5" class="title">选择银行：</el-col>
         <el-col :span="9">
-          <el-input v-model="v1"></el-input>
+          <el-select v-model="select1" placeholder="选择需要绑定的银行">
+            <el-option
+              v-for="item in select1List"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
         </el-col>
       </el-row>
       <el-row class="bd">
         <el-col :span="5" class="title">输入银行卡号码：</el-col>
         <el-col :span="9">
-          <el-input v-model="v1"></el-input>
+          <el-input v-model="v1" placeholder="输入银行卡号码"></el-input>
         </el-col>
       </el-row>
       <el-row>
         <el-col :span="5" class="title">卡类型：</el-col>
         <el-col :span="9">
-          <span class="red2">
-            <el-input v-model="v1"></el-input>
-          </span>
+          <el-select v-model="select2" placeholder="选择卡的类型">
+            <el-option
+              v-for="item in select2List"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
         </el-col>
       </el-row>
       <el-row>
         <el-col :span="5" class="title">持卡人姓名：</el-col>
         <el-col :span="9">
-          <span class="red2">
-            <el-input v-model="v1"></el-input>
-          </span>
+          <el-input v-model="v1" placeholder="输入持卡人真实姓名"></el-input>
         </el-col>
       </el-row>
       <el-row>
         <el-col :span="5" class="title">手机号码：</el-col>
         <el-col :span="9">
-          <el-input placeholder="手机号" v-model="input2">
+          <el-input placeholder="输入手机号码" v-model="input2">
             <template slot="append">发送验证码</template>
           </el-input>
         </el-col>
@@ -48,7 +58,7 @@
       <el-row>
         <el-col :span="5" class="title">验证码：</el-col>
         <el-col :span="9">
-          <el-input v-model="v3"></el-input>
+          <el-input v-model="v3" placeholder="输入手机验证码"></el-input>
         </el-col>
       </el-row>
       <div class="submit_btn_sy mbt mt">
@@ -59,9 +69,10 @@
     <el-dialog title="提示" :visible.sync="AddVisible" width="450px" class="playing_dialog_com_sy">
       <div class="info_body_dia">
         <p class="p1" v-show="isAdd">银行卡绑定成功，请注意您的交易安全！</p>
-        <p class="p2 red2"  v-show="!isAdd">银行卡绑定未成功，请重新检查输入信息并添加！</p>
+        <p class="p2 red2" v-show="!isAdd">银行卡绑定未成功，请重新检查输入信息并添加！</p>
         <div class="submit_btn_sy mbt mt orb">
-          <el-button @click="isAdd = false" v-show="isAdd">关 闭</el-button><span></span>
+          <el-button @click="isAdd = false" v-show="isAdd">关 闭</el-button>
+          <span></span>
           <el-button @click="AddVisible = false" v-show="!isAdd">返 回</el-button>
         </div>
       </div>
@@ -81,7 +92,29 @@ export default {
       checked: true,
       checked3: true,
       AddVisible: false,
-      isAdd: true
+      isAdd: true,
+      select1List: [
+        {
+          value: "选项1",
+          label: "工行"
+        },
+        {
+          value: "选项2",
+          label: "建行"
+        }
+      ],
+      select1: "",
+      select2List: [
+        {
+          value: "选项11",
+          label: "类型1"
+        },
+        {
+          value: "选项22",
+          label: "类型2"
+        }
+      ],
+      select2: ""
     };
   },
   methods: {
@@ -95,30 +128,4 @@ export default {
 };
 </script>
 <style lang="less">
-.wallet_dm_t_body {
-  color: #666;
-  padding: 30px 40px;
-  .title {
-    text-align: right;
-    font-weight: bold;
-    padding-right: 6px;
-  }
-  .el-row {
-    line-height: 32px;
-    margin-bottom: 10px;
-  }
-}
-.info_body_dia {
-  padding: 0 40px;
-  p {
-    text-align: center;
-    font-size: 16px;
-    padding: 50px 0 30px;
-  }
-}
-.seller_log_body{
-  th{
-    background-color: #eee;
-  }
-}
 </style>
